57a9afa23a860a3b7b5c4b60d3ab003e69e72f21
tesseir <tesseir.edu@gmail.com>
2023-04-10 14:51:23 -0500

    init commit for fixed repo

---
+++ src/resumeData.js
@@ -0,0 +1,116 @@
+let resumeData = {
+  "imagebaseurl": "https://github.com/tesseir",
+  "name": "Tess Eir",
+  "role": "Full Stack Web Developer",
+  "linkedinId": "www.linkedin.com/in/tesseir",
+  "skypeid": "Your skypeid",
+  "roleDescription": " Currently looking for an intern position or jr. developer position. I have a passion for all technologies, hardware and software.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/tess-eir-6a3b69267/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/tesseir",
+      "className": "fa fa-github"
+    },
+  ],
+  "aboutme": "I graduated from the University of Minnesota Coding Bootcamp in March of 2023. I am passionate about learning new technologies and solving problems. I am also a good communicator and a team player. I currently after my AWS Cloud Practitioners Certificate.",
+  "address": "USA",
+  "website": "",
+  "education": [
+    {
+      "UniversityName": "University of Minnesota Coding Bootcamp",
+      "specialization": "Full Stack Web Development",
+      "MonthOfPassing": "March",
+      "YearOfPassing": "2023",
+      "Achievements": "Some Achievements"
+    },
+  ],
+  "work": [
+    {
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "skillsDescription": "What im good at!",
+  "skills": [
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Expressjs"
+    },
+    {
+      "skillname": "Nodejs"
+    },
+    {
+      "skillname": "MySQL"
+    },
+    {
+      "skillname": "NoSQL"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "E-commerce API",
+      "description": "E-commerce API",
+      "imgurl": "images/portfolio/e-commerce.png",
+      "githuburl": "https://github.com/tesseir/e-commerce"
+    },
+    {
+      "name": "Note Taker App",
+      "description": "Note Taker App",
+      "imgurl": "images/portfolio/note-taker.png",
+      "githuburl": "https://github.com/tesseir/note-taker"
+    },
+    {
+      "name": "Project Tracker",
+      "description": "Project Tracker App using Handlebars",
+      "imgurl": "images/portfolio/project-Tracker.png",
+      "githuburl": "https://github.com/tesseir/project-tracker"
+    },
+    {
+      "name": "Readme Generator",
+      "description": "A simple Readme Generator",
+      "imgurl": "images/portfolio/readme-generator.png",
+      "githuburl": "https://github.com/tesseir/readme-generator"
+    },
+    {
+      "name": "Social Media API",
+      "description": "A social media API using MongoDB, Mongoose, and Express",
+      "imgurl": "images/portfolio/social-media-api.png",
+      "githuburl": "https://github.com/tesseir/social-media-api"
+    },
+    {
+      "name": "workday scheduler",
+      "description": "A simple workday scheduler",
+      "imgurl": "images/portfolio/workday-scheduler.png",
+      "githuburl": "https://github.com/tesseir/Workday-Scheduler"
+    },
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
