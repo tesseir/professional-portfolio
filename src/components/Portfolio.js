9cfd92c77e80ba90c5efe2992e1baa9a71b22013
tesseir <tesseir.edu@gmail.com>
2023-04-11 12:02:56 -0500

    added script for github pages

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,7 +8,7 @@ export default class Porfolio extends Component {
           <div className="twelve columns collapsed">
             <h1>Check Out Some of My Works.</h1>
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-              {/* {
+              {
                 resumeData.portfolio && resumeData.portfolio.map((item) => {
                   return (
                     <div key={item.url} className="columns portfolio-item">
@@ -26,23 +26,23 @@ export default class Porfolio extends Component {
                     </div>
                   )
                 })
-              } */}
+              }
 
 {/* hard code test section */}
 
-              <div key="test" className="columns portfolio-item">
+              {/* <div key="test" className="columns portfolio-item">
                 <div className="item-wrap">
                   <a href="https://github.com/tesseir/e-commerce" target='_blank'>
                     <img src="images/portfolio/e-commerce.png" className="item-img" />
-                    {/* <div className="overlay">
+                    <div className="overlay">
                       <div className="portfolio-item-meta">
                         <h5>Click Here to View Repo</h5>
                         <p>E-commerce API</p>
                       </div>
-                    </div> */}
+                    </div>
                   </a>
                 </div>
-              </div>
+              </div> */}
 
             </div>
           </div>
